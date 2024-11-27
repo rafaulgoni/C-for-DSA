40f5535bac2e3b5d9b2ea177671d8faa79625b69
unknown <rafaulgoni787898@gmail.com>
2024-11-28 02:09:24 +0600

    done problem

--- code.cpp
+++ code.cpp
@@ -1,9 +1,33 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
+struct Student {
+    int id;
+    string name;
+    char section;
+    int marks;
+};
+
 int main() {
-    int x;
-    cin >> x;
-    cout << x; 
+    int T; 
+    cin >> T;
+
+    while (T--) {
+        Student students[3];
+
+        for (int i = 0; i < 3; i++) {
+            cin >> students[i].id >> students[i].name >> students[i].section >> students[i].marks;
+        }
+
+        Student* best = &students[0];
+        for (int i = 1; i < 3; i++) {
+            if (students[i].marks > best->marks || 
+               (students[i].marks == best->marks && students[i].id < best->id)) {
+                best = &students[i]; 
+            }
+        }
+        cout << best->id << " " << best->name << " " << best->section << " " << best->marks << endl;
+    }
+
     return 0;
 }
